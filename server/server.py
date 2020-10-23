0abc1c8f3e9392b46c501e2ef0220b8bc313ad1c
Suarez94 <61251655+Suarez94@users.noreply.github.com>
2020-10-23 18:07:40 +0200

    Update server.py

--- server/server.py
+++ server/server.py
@@ -88,67 +88,53 @@ def upload_image():
       img = np.array(imgage2) 
       img = img[:, :, ::-1].copy() 
 
-      
-      
-      #making prediction#
-    #first cropp image 
+      #getting gray copy
       img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
       image = cv2.equalizeHist(img_gray)
              
-
+      # lungs segmentation
       image_c = img_gray
       image_c = cv2.resize(image_c, (512,512), interpolation = cv2.INTER_AREA)
       image_c = np.array(image_c) / 255
       image_c = image_c.astype('float32')
       image_to_predict = image_c.reshape(1, 512, 512,1)
       predict_img = model_croping.predict(image_to_predict)
+        
+      # creating mask of lungs 
       mask = predict_img[0,:,:,0]
-
       mask = cv2.medianBlur(mask, 3)
-
       mask = skimage.morphology.opening(mask,disk(10))
       mask = skimage.morphology.dilation(mask, disk(20))
       mask = cv2.resize(mask, (224,224), interpolation = cv2.INTER_AREA)
 
-
+      # creating heatmap 
       image_size = 224
       image = cv2.cvtColor(image,cv2.COLOR_GRAY2RGB)
       image = cv2.resize(image, (224,224), interpolation = cv2.INTER_AREA)
       x_test = np.array(image) / 255
       x_test = x_test.reshape(-1, image_size, image_size,3)
       x_test = x_test.astype('float32')
-
       dim = (image_size, image_size)
-      # resize image
       image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-
-
       img_tensor = x_test
       layer = 'conv2d_5'
       conv_layer = model.get_layer(layer)
-
-      heatmap_model = tf.keras.models.Model(
-          [model.inputs], [model.get_layer(layer).output, model.output]
-      )
-
+      heatmap_model = tf.keras.models.Model([model.inputs], [model.get_layer(layer).output, model.output])
       with tf.GradientTape() as tape:
           conv_output, predictions = heatmap_model(img_tensor)
           loss = predictions[:, np.argmax(predictions[0])]
-
       grads = tape.gradient(loss, conv_output)
       pooled_grads = K.mean(grads, axis=(0, 1, 2))
-
       heatmap = tf.reduce_mean(tf.multiply(pooled_grads, conv_output), axis=-1)
-
       heatmap = np.maximum(heatmap, 0)
       max_heat = np.max(heatmap)
-
       if max_heat == 0:
           max_heat = 1e-10
       heatmap /= max_heat
       heatmap = heatmap[0,:,:]
       heatmap = cv2.resize(heatmap, dim, interpolation = cv2.INTER_AREA)
-
+    
+      # making prediction of disease
       prediction = model.predict(x_test)
       pred = np.argmax(prediction,axis=1)
       probability = prediction.max()*100
@@ -160,21 +146,20 @@ def upload_image():
       if pred[0] == 2:
           result = 'Pneumonia'
         
-
+      # creating result image
       mask[mask > 0.5] = 1
       mask[mask <= 0.5] = 0
       result_image = image
-
       result_image[:,:,0] = result_image[:,:,0]*mask
       result_image[:,:,1] = result_image[:,:,1]*mask
       result_image[:,:,2] = result_image[:,:,2]*mask
       result_image[:,:,0]=image[:,:,1]*(1-heatmap) 
       result_image[:,:,1]=image[:,:,1]*heatmap 
-      
       result_image2 = result_image
       result_image2[:,:,2] = result_image[:,:,0]
       result_image2[:,:,0] = result_image[:,:,1]
-      ##########################    
+      
+      # creating message to send  
       items = []
       items = {'name': result, 'prob': probability}
       response = {'pred':items}
